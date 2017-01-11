59a13d2c18a362e298cd6f4a65f7c3c02d4c21c3
RubyChou <441054947@qq.com>
2017-01-11 17:36:57 +0800

    Modify javadoc

--- data-driver/src/main/java/com/sagittarius/read/Reader.java
+++ data-driver/src/main/java/com/sagittarius/read/Reader.java
@@ -9,64 +9,64 @@ import java.util.Map;
 
 public interface Reader {
     /**
-     * given hosts lists and metrics lists , get IntPoints at the query time.
-     * @param hosts lists of hosts,namely devices
-     * @param metrics lists of metrics, namely sensors
+     * given hosts list and metrics list,each host is associated with the same list of metrics , get IntPoints at the query time.
+     * @param hosts lists of hosts
+     * @param metrics lists of metrics
      * @param time query time
      * @return map of IntPoints at the query time, the key is  host name, the value is list of IntPoints related to that host
      */
     Map<String, List<IntPoint>> getIntPoint(List<String> hosts, List<String> metrics, long time);
     /**
-     * given hosts lists and metrics lists , get LongPoints at the query time.
-     * @param hosts lists of hosts,namely devices
-     * @param metrics lists of metrics, namely sensors
+     * given hosts list and metrics list,each host is associated with the same list of metrics , get LongPoints at the query time.
+     * @param hosts lists of hosts
+     * @param metrics lists of metrics
      * @param time query time
      * @return map of LongPoints at the query time, the key is  host name, the value is list of LongPoints related to that host
      */
     Map<String, List<LongPoint>> getLongPoint(List<String> hosts, List<String> metrics, long time);
     /**
-     * given hosts lists and metrics lists , get FloatPoints at the query time.
-     * @param hosts lists of hosts,namely devices
-     * @param metrics lists of metrics, namely sensors
+     * given hosts list and metrics list,each host is associated with the same list of metrics , get FloatPoints at the query time.
+     * @param hosts lists of hosts
+     * @param metrics lists of metrics
      * @param time query time
      * @return map of FloatPoints at the query time, the key is  host name, the value is list of FloatPoints related to that host
      */
     Map<String, List<FloatPoint>> getFloatPoint(List<String> hosts, List<String> metrics, long time);
     /**
-     * given hosts lists and metrics lists , get DoublePoints at the query time.
-     * @param hosts lists of hosts,namely devices
-     * @param metrics lists of metrics, namely sensors
+     * given hosts list and metrics list,each host is associated with the same list of metrics , get DoublePoints at the query time.
+     * @param hosts lists of hosts
+     * @param metrics lists of metrics
      * @param time query time
      * @return map of DoublePoints at the query time, the key is  host name, the value is list of DoublePoints related to that host
      */
     Map<String, List<DoublePoint>> getDoublePoint(List<String> hosts, List<String> metrics, long time);
     /**
-     * given hosts lists and metrics lists , get BooleanPoints at the query time.
-     * @param hosts lists of hosts,namely devices
-     * @param metrics lists of metrics, namely sensors
+     * given hosts list and metrics list,each host is associated with the same list of metrics , get BooleanPoints at the query time.
+     * @param hosts lists of hosts
+     * @param metrics lists of metrics
      * @param time query time
      * @return map of BooleanPoints at the query time, the key is  host name, the value is list of BooleanPoints related to that host
      */
     Map<String, List<BooleanPoint>> getBooleanPoint(List<String> hosts, List<String> metrics, long time);
     /**
-     * given hosts lists and metrics lists , get StringPoints at the query time.
-     * @param hosts lists of hosts,namely devices
-     * @param metrics lists of metrics, namely sensors
+     * given hosts list and metrics list,each host is associated with the same list of metrics , get StringPoints at the query time.
+     * @param hosts lists of hosts
+     * @param metrics lists of metrics
      * @param time query time
      * @return map of StringPoints at the query time, the key is  host name, the value is list of StringPoints related to that host
      */
     Map<String, List<StringPoint>> getStringPoint(List<String> hosts, List<String> metrics, long time);
     /**
-     * given hosts lists and metrics lists , get GeoPoints at the query time.
-     * @param hosts lists of hosts,namely devices
-     * @param metrics lists of metrics, namely sensors
+     * given hosts list and metrics list,each host is associated with the same list of metrics , get GeoPoints at the query time.
+     * @param hosts lists of hosts
+     * @param metrics lists of metrics
      * @param time query time
      * @return map of GeoPoints at the query time, the key is  host name, the value is list of GeoPoints related to that host
      */
     Map<String, List<GeoPoint>> getGeoPoint(List<String> hosts, List<String> metrics, long time);
 
     /**
-     * given host and metric , get a IntPoint at the query time, if there isn't point at that time, searching point according to shift option.
+     * given host and metric , get a IntPoint at the query time, if there isn't point at that time, will search point according to shift option.
      * the supported shifts are:
      * <br>
      * <br>       BEFORE -- search backward for a point nearest to the query time
@@ -80,7 +80,7 @@ public interface Reader {
      */
     IntPoint getFuzzyIntPoint(String host, String metric, long time, Shift shift);
     /**
-     * given host and metric , get a LongPoint at the query time, if there isn't point at that time, searching point according to shift option.
+     * given host and metric , get a LongPoint at the query time, if there isn't point at that time, will search point according to shift option.
      * the supported shifts are:
      * <br>
      * <br>       BEFORE -- search backward for a point nearest to the query time
@@ -94,7 +94,7 @@ public interface Reader {
      */
     LongPoint getFuzzyLongPoint(String host, String metric, long time, Shift shift);
     /**
-     * given host and metric , get a FloatPoint at the query time, if there isn't point at that time, searching point according to shift option.
+     * given host and metric , get a FloatPoint at the query time, if there isn't point at that time, will search point according to shift option.
      * the supported shifts are:
      * <br>
      * <br>       BEFORE -- search backward for a point nearest to the query time
@@ -108,7 +108,7 @@ public interface Reader {
      */
     FloatPoint getFuzzyFloatPoint(String host, String metric, long time, Shift shift);
     /**
-     * given host and metric , get a DoublePoint at the query time, if there isn't point at that time, searching point according to shift option.
+     * given host and metric , get a DoublePoint at the query time, if there isn't point at that time, will search point according to shift option.
      * the supported shifts are:
      * <br>
      * <br>       BEFORE -- search backward for a point nearest to the query time
@@ -122,7 +122,7 @@ public interface Reader {
      */
     DoublePoint getFuzzyDoublePoint(String host, String metric, long time, Shift shift);
     /**
-     * given host and metric , get a BooleanPoint at the query time, if there isn't point at that time, searching point according to shift option.
+     * given host and metric , get a BooleanPoint at the query time, if there isn't point at that time, will search point according to shift option.
      * the supported shifts are:
      * <br>
      * <br>       BEFORE -- search backward for a point nearest to the query time
@@ -136,7 +136,7 @@ public interface Reader {
      */
     BooleanPoint getFuzzyBooleanPoint(String host, String metric, long time, Shift shift);
     /**
-     * given host and metric , get a StringPoint at the query time, if there isn't point at that time, searching point according to shift option.
+     * given host and metric , get a StringPoint at the query time, if there isn't point at that time, will search point according to shift option.
      * the supported shifts are:
      * <br>
      * <br>       BEFORE -- search backward for a point nearest to the query time
@@ -150,7 +150,7 @@ public interface Reader {
      */
     StringPoint getFuzzyStringPoint(String host, String metric, long time, Shift shift);
     /**
-     * given host and metric , get a GeoPoint at the query time, if there isn't point at that time, searching point according to shift option.
+     * given host and metric , get a GeoPoint at the query time, if there isn't point at that time, will search point according to shift option.
      * the supported shifts are:
      * <br>
      * <br>       BEFORE -- search backward for a point nearest to the query time
@@ -164,112 +164,112 @@ public interface Reader {
      */
     GeoPoint getFuzzyGeoPoint(String host, String metric, long time, Shift shift);
     /**
-     * given hosts lists and metrics lists , get IntPoints at the latest time.
-     * @param hosts lists of hosts,namely devices
-     * @param metrics lists of metrics, namely sensors
+     * given hosts list and metrics list,each host is associated with the same list of metrics , get IntPoints at the latest time.
+     * @param hosts lists of hosts
+     * @param metrics lists of metrics
      * @return map of IntPoints at the latest time, the key is  host name, the value is list of IntPoints related to that host
      */
     Map<String, List<IntPoint>> getIntLatest(List<String> hosts, List<String> metrics);
     /**
-     * given hosts lists and metrics lists , get LongPoints at the latest time.
-     * @param hosts lists of hosts,namely devices
-     * @param metrics lists of metrics, namely sensors
+     * given hosts list and metrics list,each host is associated with the same list of metrics , get LongPoints at the latest time.
+     * @param hosts lists of hosts
+     * @param metrics lists of metrics
      * @return map of LongPoints at the latest time, the key is  host name, the value is list of LongPoints related to that host
      */
     Map<String, List<LongPoint>> getLongLatest(List<String> hosts, List<String> metrics);
     /**
-     * given hosts lists and metrics lists , get FloatPoints at the latest time.
-     * @param hosts lists of hosts,namely devices
-     * @param metrics lists of metrics, namely sensors
+     * given hosts list and metrics list,each host is associated with the same list of metrics , get FloatPoints at the latest time.
+     * @param hosts lists of hosts
+     * @param metrics lists of metrics
      * @return map of FloatPoints at the latest time, the key is  host name, the value is list of FloatPoints related to that host
      */
     Map<String, List<FloatPoint>> getFloatLatest(List<String> hosts, List<String> metrics);
     /**
-     * given hosts lists and metrics lists , get DoublePoints at the latest time.
-     * @param hosts lists of hosts,namely devices
-     * @param metrics lists of metrics, namely sensors
+     * given hosts list and metrics list,each host is associated with the same list of metrics , get DoublePoints at the latest time.
+     * @param hosts lists of hosts
+     * @param metrics lists of metrics
      * @return map of DoublePoints at the latest time, the key is  host name, the value is list of DoublePoints related to that host
      */
     Map<String, List<DoublePoint>> getDoubleLatest(List<String> hosts, List<String> metrics);
     /**
-     * given hosts lists and metrics lists , get BooleanPoints at the latest time.
-     * @param hosts lists of hosts,namely devices
-     * @param metrics lists of metrics, namely sensors
+     * given hosts list and metrics list,each host is associated with the same list of metrics , get BooleanPoints at the latest time.
+     * @param hosts lists of hosts
+     * @param metrics lists of metrics
      * @return map of BooleanPoints at the latest time, the key is  host name, the value is list of BooleanPoints related to that host
      */
     Map<String, List<BooleanPoint>> getBooleanLatest(List<String> hosts, List<String> metrics);
     /**
-     * given hosts lists and metrics lists , get StringPoints at the latest time.
-     * @param hosts lists of hosts,namely devices
-     * @param metrics lists of metrics, namely sensors
+     * given hosts list and metrics list,each host is associated with the same list of metrics , get StringPoints at the latest time.
+     * @param hosts lists of hosts
+     * @param metrics lists of metrics
      * @return map of StringPoints at the latest time, the key is  host name, the value is list of StringPoints related to that host
      */
     Map<String, List<StringPoint>> getStringLatest(List<String> hosts, List<String> metrics);
     /**
-     * given hosts lists and metrics lists , get GeoPoints at the latest time.
-     * @param hosts lists of hosts,namely devices
-     * @param metrics lists of metrics, namely sensors
+     * given hosts list and metrics list,each host is associated with the same list of metrics , get GeoPoints at the latest time.
+     * @param hosts lists of hosts
+     * @param metrics lists of metrics
      * @return map of GeoPoints at the latest time, the key is  host name, the value is list of GeoPoints related to that host
      */
     Map<String, List<GeoPoint>> getGeoLatest(List<String> hosts, List<String> metrics);
     /**
-     * given hosts lists and metrics lists , get IntPoints in the given time range.
-     * @param hosts lists of hosts,namely devices
-     * @param metrics lists of metrics, namely sensors
+     * given hosts list and metrics list,each host is associated with the same list of metrics , get IntPoints in the given time range.
+     * @param hosts lists of hosts
+     * @param metrics lists of metrics
      * @param startTime start time of the range
      * @param endTime end time of the range
      * @return map of IntPoints, the key is  host name, the value is list of IntPoints related to that host
      */
     Map<String, List<IntPoint>> getIntRange(List<String> hosts, List<String> metrics, long startTime, long endTime);
     /**
-     * given hosts lists and metrics lists , get LongPoints in the given time range.
-     * @param hosts lists of hosts,namely devices
-     * @param metrics lists of metrics, namely sensors
+     * given hosts list and metrics list,each host is associated with the same list of metrics , get LongPoints in the given time range.
+     * @param hosts lists of hosts
+     * @param metrics lists of metrics
      * @param startTime start time of the range
      * @param endTime end time of the range
      * @return map of LongPoints, the key is  host name, the value is list of LongPoints related to that host
      */
     Map<String, List<LongPoint>> getLongRange(List<String> hosts, List<String> metrics, long startTime, long endTime);
     /**
-     * given hosts lists and metrics lists , get FloatPoints in the given time range.
-     * @param hosts lists of hosts,namely devices
-     * @param metrics lists of metrics, namely sensors
+     * given hosts list and metrics list,each host is associated with the same list of metrics , get FloatPoints in the given time range.
+     * @param hosts lists of hosts
+     * @param metrics lists of metrics
      * @param startTime start time of the range
      * @param endTime end time of the range
      * @return map of FloatPoints, the key is  host name, the value is list of FloatPoints related to that host
      */
     Map<String, List<FloatPoint>> getFloatRange(List<String> hosts, List<String> metrics, long startTime, long endTime);
     /**
-     * given hosts lists and metrics lists , get DoublePoints in the given time range.
-     * @param hosts lists of hosts,namely devices
-     * @param metrics lists of metrics, namely sensors
+     * given hosts list and metrics list,each host is associated with the same list of metrics , get DoublePoints in the given time range.
+     * @param hosts lists of hosts
+     * @param metrics lists of metrics
      * @param startTime start time of the range
      * @param endTime end time of the range
      * @return map of DoublePoints, the key is  host name, the value is list of DoublePoints related to that host
      */
     Map<String, List<DoublePoint>> getDoubleRange(List<String> hosts, List<String> metrics, long startTime, long endTime);
     /**
-     * given hosts lists and metrics lists , get BooleanPoints in the given time range.
-     * @param hosts lists of hosts,namely devices
-     * @param metrics lists of metrics, namely sensors
+     * given hosts list and metrics list,each host is associated with the same list of metrics , get BooleanPoints in the given time range.
+     * @param hosts lists of hosts
+     * @param metrics lists of metrics
      * @param startTime start time of the range
      * @param endTime end time of the range
      * @return map of BooleanPoints, the key is  host name, the value is list of BooleanPoints related to that host
      */
     Map<String, List<BooleanPoint>> getBooleanRange(List<String> hosts, List<String> metrics, long startTime, long endTime);
     /**
-     * given hosts lists and metrics lists , get StringPoints in the given time range.
-     * @param hosts lists of hosts,namely devices
-     * @param metrics lists of metrics, namely sensors
+     * given hosts list and metrics list,each host is associated with the same list of metrics , get StringPoints in the given time range.
+     * @param hosts lists of hosts
+     * @param metrics lists of metrics
      * @param startTime start time of the range
      * @param endTime end time of the range
      * @return map of StringPoints, the key is  host name, the value is list of StringPoints related to that host
      */
     Map<String, List<StringPoint>> getStringRange(List<String> hosts, List<String> metrics, long startTime, long endTime);
     /**
-     * given hosts lists and metrics lists , get GeoPoints in the given time range.
-     * @param hosts lists of hosts,namely devices
-     * @param metrics lists of metrics, namely sensors
+     * given hosts list and metrics list,each host is associated with the same list of metrics , get GeoPoints in the given time range.
+     * @param hosts lists of hosts
+     * @param metrics lists of metrics
      * @param startTime start time of the range
      * @param endTime end time of the range
      * @return map of GeoPoints, the key is  host name, the value is list of GeoPoints related to that host
